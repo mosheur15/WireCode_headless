c1fae7a4ff2130233819ae46caa81f5216bb892c
mosheur15 <mosheur.rahman.15@gmail.com>
2024-04-07 03:58:44 +0600

    added nav and sidebar

--- index.js
+++ index.js
@@ -1,10 +1,36 @@
 'use strict'
+
 const editor = ace.edit("editor")
 editor.setTheme('ace/theme/tomorrow_night')
 editor.session.setMode('ace/mode/javascript')
+editor.resize(true)
 editor.setOptions({
     enableLiveAutocompletion: true
 })
 
-// turn off inline warnings for gutter width fix.
+// turn off inline warnings for gutter.
+// because width fix makes it stick in top of line number.
 editor.getSession().setUseWorker(false)
+
+
+let toggle = document.getElementById("toggle")
+let sidebar = document.getElementById('sidebar')
+let gutter = document.querySelector(".ace_gutter")
+
+console.log("gutter")
+console.log(gutter)
+
+let isToggled = true
+
+toggle.onclick = ()=>{
+    if (!isToggled){
+        gutter.style.display = "block"
+        sidebar.style.display = "none"
+        isToggled = true
+        return
+    }
+    
+    gutter.style.display = "none"
+    sidebar.style.display = "block"
+    isToggled = false
+}
